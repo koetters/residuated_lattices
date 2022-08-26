3d3dc2c67b1971c98923491f4b33c32065728d6f
Jens Koetters <jkoetters5@gmail.com>
2022-08-26 20:05:17 +0200

    ...

--- example1_8.py
+++ example1_8.py
@@ -1,14 +1,19 @@
 from residuated_lattice import ResiduatedLattice
 
+# The definitions below are given for convenience, so we can easily refer to elements of the algebra.
 a="a"
 b="b"
 c="c"
 d="d"
 
+# The domain (or base set) of the algebra. The order in which the elements are stated is important:
+# the list serves as a column header and row header for the operation tables below.
 domain = [0,a,b,c,d,1]
 
+# The "line diagram" of the lattice order: only the neighboring pairs in the order have to be given.
 line_diag = [(0,b),(b,a),(a,1),(0,d),(d,c),(c,a)]
 
+# The operation table for the arrow operation.
 arrow_matrix = [
     [1,1,1,1,1,1],
     [0,1,b,c,c,1],
@@ -18,6 +23,7 @@ arrow_matrix = [
     [0,a,b,c,d,1],
 ]
 
+# The operation table for the multiplication.
 prod_matrix = [
     [0,0,0,0,0,0],
     [0,a,b,d,d,a],
